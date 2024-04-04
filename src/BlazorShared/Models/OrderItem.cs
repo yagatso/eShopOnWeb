350edd230c5becedb4e5191ab1def3fc235cd12b
Yiğit Bektaş <yagatso@hotmail.com>
2024-04-04 18:02:29 +0300

    init

---
+++ src/BlazorShared/Models/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace BlazorShared.Models;
+
+public class OrderItem
+{
+    public int orderId { get; set; }
+    public DateTime date { get; set; }
+    public decimal total { get; set; }
+    public string status { get; set; }
+}
+
+public class OrderItemDetail
+{
+    public int orderId { get; set; }
+    public int unit { get; set; }
+    public decimal price { get; set; }
+    public string name { get; set; }
+
+    
+}
